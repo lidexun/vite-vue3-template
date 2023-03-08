eea4eed479efffe7642abc54409830dcc6459ba4
lidexun <910547462@qq.com>
2023-03-08 18:01:41 +0800

    update:  vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 9527, // 设置服务启动端口号
+    open: false, // 设置服务启动时是否自动打开浏览器
+    cors: true,
+    proxy: {},
+  }
 })
