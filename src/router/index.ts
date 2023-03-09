cf13b3c7210330280941c93bc99e3ca68cd7fe66
lidexun <910547462@qq.com>
2023-03-09 22:56:52 +0800

    feat: router配置自导生成路由

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
+import routes from '~pages'
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior() {
+    return {
+      el: '#app',
+      top: 0,
+      behavior: 'smooth',
+    };
+  }
+})
+
+export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [
+    vue(),
+    Pages({
+      importMode: 'async',
+      // 路由风格nuxtjs
+      routeStyle: 'nuxt',
+      // 排除components下的路由
+      exclude: ['**/components/*.vue']
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
   },
   server: {
-    port: 9527, // 设置服务启动端口号
-    open: false, // 设置服务启动时是否自动打开浏览器
+    // 是否开启 https
+    https: false,
+    // 设置服务启动端口号
+    port: 9527,
+    // 设置服务启动时是否自动打开浏览器
+    open: false,
+    // 允许跨域
     cors: true,
+    // 自定义代理
     proxy: {},
   }
 })
